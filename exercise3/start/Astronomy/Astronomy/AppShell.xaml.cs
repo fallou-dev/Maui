f377b574773a389d401a5e8003d7d4458880da87
fallo <serignefalloundiaye6@esp.sn>
2022-12-08 23:33:07 +0000

    Ajoutez des fichiers projet.

---
+++ exercise3/start/Astronomy/Astronomy/AppShell.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Astronomy.Pages;
+
+namespace Astronomy;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        Routing.RegisterRoute("astronomicalbodydetails", typeof(AstronomicalBodyPage));
+    }
+    [QueryProperty(nameof(AstroName), "astroName")]
+
+    public partial class AstronomicalBodyPage
+    {
+        InitializeComponent();
+
+        btnComet.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails");
+        btnEarth.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails");
+        btnMoon.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails");
+        btnSun.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails");
+    
+    
+    }
+    string astroName;
+    public string AstroName
+    {
+        get => astroName;
+        set
+        {
+            astroName = value;
+
+            // this is a custom function to update the UI immediately
+            UpdateAstroBodyUI(astroName);
+        }
+    }
+
+
+}
